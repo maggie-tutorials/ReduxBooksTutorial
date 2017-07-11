49943d1b6778e8ec7b78606b0cf738f033d05a35
Maggie Serino <maggie.dot.magpie@gmail.com>
2017-07-11 15:21:24 +0200

    Adding input fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input} />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        PostsNew
+        <form>
+          <Field
+            label="Title"
+            name="title"
+            component={this.renderField} />
+          <Field
+            label="Tags"
+            name="tags"
+            component={this.renderField} />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField} />
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm' // this needs to be unique in the application
+})(PostsNew);
